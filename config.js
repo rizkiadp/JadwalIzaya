286ccf757e5d3f89b78cc206e0d1b1c28e9eff3a
Rizki <emailanda@example.com>
2026-02-17 20:49:25 +0700

    Fix config url

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
     // Ganti ini dengan URL Backend Vercel kamu setelah deploy
     // Contoh: 'https://jadwalizayabackend.vercel.app'
-    API_BASE_URL: 'https://jadwalizayabackend.vercel.app/'
+    API_BASE_URL: 'https://jadwalizayabackend.vercel.app'
 };
